951870dbfc7f97f121abb677644c02cc0cf8c855
hupeng <xiaomo@xiaomo.info>
2018-11-17 15:07:45 +0800

    背包界面

--- Assets/Scripts/UI/MainUIManager.cs
+++ Assets/Scripts/UI/MainUIManager.cs
@@ -5,11 +5,23 @@ namespace UI
     public class MainUIManager : MonoBehaviour
     {
         public RectTransform EquipFrom;
+        public RectTransform BagFrom;
 
 
         public void ToggleEquip()
         {
-            EquipFrom.gameObject.SetActive(!EquipFrom.gameObject.active);
+            EquipFrom.gameObject.SetActive(!EquipFrom.gameObject.activeSelf);
+        }
+
+        public void ToggleBag()
+        {
+            BagFrom.gameObject.SetActive(!BagFrom.gameObject.activeSelf);
+        }
+
+
+        public void CloseBag()
+        {
+            BagFrom.gameObject.SetActive(false);
         }
     }
 }
