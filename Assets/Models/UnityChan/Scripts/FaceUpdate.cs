0efa9a9fbc799b74af3d45a675aeb99763d5eca7
xiaomo <xiaomo@xiamo.info>
2018-11-18 15:39:01 +0800

    添加人物

--- Assets/Models/UnityChan/Scripts/FaceUpdate.cs
+++ Assets/Models/UnityChan/Scripts/FaceUpdate.cs
@@ -3,37 +3,40 @@ using System.Collections;
 
 public class FaceUpdate : MonoBehaviour
 {
-	public AnimationClip[] animations;
+    public AnimationClip[] animations;
 
-	Animator anim;
+    Animator anim;
 
-	public float delayWeight;
+    public float delayWeight;
 
-	void Start ()
-	{
-		anim = GetComponent<Animator> ();
-	}
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
-	void OnGUI ()
-	{
-		foreach (var animation in animations) {
-			if (GUILayout.Button (animation.name)) {
-				anim.CrossFade (animation.name, 0);
-			}
-		}
-	}
+//	void OnGUI ()
+//	{
+//		foreach (var animation in animations) {
+//			if (GUILayout.Button (animation.name)) {
+//				anim.CrossFade (animation.name, 0);
+//			}
+//		}
+//	}
 
-	float current = 0;
+    float current = 0;
 
 
-	void Update ()
-	{
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            current = 1;
+        }
+        else
+        {
+            current = Mathf.Lerp(current, 0, delayWeight);
+        }
 
-		if (Input.GetMouseButton (0)) {
-			current = 1;
-		} else {
-			current = Mathf.Lerp (current, 0, delayWeight);
-		}
-		anim.SetLayerWeight (1, current);
-	}
+        anim.SetLayerWeight(1, current);
+    }
 }
